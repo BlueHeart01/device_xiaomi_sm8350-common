008d24e2b3e272fc09cb17acea34d2e3b0605f73
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2025-02-14 23:48:24 +0600

    sm8350-common: Remove custom task_profiles
    We'll use from system_core since our API is 31 now, which has
    uclamp support.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- common.mk
+++ common.mk
@@ -390,7 +390,6 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
     $(LOCAL_PATH)/configs/perf/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
